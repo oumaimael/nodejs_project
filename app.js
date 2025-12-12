6cafac4fe172565403135eac3a78ab06907d20fd
oumaima <oumaima.elhabib@gmail.com>
2025-12-12 14:34:52 +0100

    added the simple manipulations CRUD of a database in app.js

--- app.js
+++ app.js
@@ -20,8 +20,99 @@ const pool = mysql.createPool({
     queueLimit : 0, 
 });
 
-app.get("/cats", (req, res) => {
+//get cat from db
 
+app.get("/cat", (req, res) => {
+    pool.getConnection((err, connection) => {
+        if(err){
+            console.error("DB connection error:", err);
+            return res.status(500).json({error: "DB connection error"});
+        }
+        connection.query("SELECT * From cat", (qErr, rows) => {
+            connection.release();
+            if (qErr){
+                console.error("Query error:", qErr);
+                return res.status(500).json({eooor: "Query error"});
+            }
+            res.json(rows);
+        });
+    });
+});
+
+//get cat by id
+app.get("/cat/:id", (req, res) =>{
+    pool.getConnection((err, connection) => {
+        if (err){
+            console.error("DB connection error:", err);
+            return res.status(500).json({ error: "DB connection error"});
+        }
+        connection.query("SELECT * FROM cat where id = ?", [reportError.params.id], (qErr, rows) => {
+            const sql = "SELECT * FROM cat WHERE is = " + req.query.id;
+            //SELECT * FROM cat WHERE id = '5; DROP TABLE cat'
+            connection.release();
+            if(qErr){
+                console.error("Query error:", qErr);
+                return res.status(500).json({error: "Query error"});
+            }
+            res.json(rows);
+        });
+    });
+});
+
+//add a cat
+app.post("/cats", (req, res) => {
+    const {name, tag, description, img}  = req.body;
+    pool.getConnection((err, connection) => {
+        if(err) {
+            console.error("DB connection error:", err);
+            return res.status(500).json({error : "DB connection error"});
+        }
+        connection.query("INSERT INTO cats (name, tag, description, img) VALUES (?, ?, ?, ?)", [name, tag, description, img], (qErr, rows) => {
+            connection.release();
+            if(qErr) {
+                console.error("Query error:", qErr);
+                return res.status(500).json({error: "Query error"});
+            }
+            res.json(rows);
+        })
+    })
+});
+//delete record
+app.delete("/cat/:id", (req, res) =>{
+    pool.connection((err, connection) => {
+        if (err){
+            console.error("DB connection error:", err);
+            return res.status(500).json({ error: "DB connection error"});
+        }
+        connection.query("DELETE FROM cat where id = ?", [req.params.id], (qErr, rows) => {
+            connection.release();
+            if(qErr){
+                console.error("Query error", qErr);
+                return res.status(500).json({ error: "Query error" });
+            }
+            res.json({message: `record Num : ${req.params.id} deleted successfully`});
+        });
+    });
+});
+
+
+// Update cat
+app.put("/cats/:id", (req, res) => {
+    const {name, tag, description, img}  = req.body;
+    pool.getConnection((err, connection) => {
+        if(err) {
+            console.error("DB connection error:", err);
+            return res.status(500).json({error : "DB connection error"})
+        }
+        connection.query("UPDATE cats SET name = ?, tag = ?, description = ? WHERE id = ?", [name, tag, description, req.params.id], (qErr, rows) => {
+            connection.release();
+            if(qErr) {
+                console.error("Query error:", qErr);
+                return res.status(500).json({error: "Query error"});
+            }
+            res.json(`Message: Record Num : ${req.params.id} updated successfully`);
+        })
+    })
 });
 
 //listen to port
